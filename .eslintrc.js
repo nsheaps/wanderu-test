91a35fe1678d8e7d415b713a0c0a141922b063c6
nsheaps <nsheaps@gmail.com>
2019-10-03 19:37:42 -0400

    init commit of configs

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = { 
+  "extends": "airbnb-base",
+  "rules": {
+    "array-element-newline": ["error", "consistent"],
+    "indent": ["error", 2],
+    "no-plusplus": "off",
+    "no-await-in-loop": "off"
+  } 
+}
